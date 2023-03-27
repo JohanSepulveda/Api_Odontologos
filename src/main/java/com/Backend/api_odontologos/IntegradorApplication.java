a804184c62e33f408ef7d62a4e5d7d346e7bce2b
Johan <jsepulv28@hotmail.com>
2023-03-27 12:57:30 -0500

    001-Creación de la estructuera básica del proyecto

---
+++ src/main/java/com/Backend/api_odontologos/IntegradorApplication.java
@@ -0,0 +1,15 @@
+package com.Backend.clase23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradorApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(IntegradorApplication.class, args);
+	}
+
+
+
+}
---
+++ src/main/resources/static/update/update_turno.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', function () {
+
+
+    const formulario = document.querySelector('#update_turno_form');
+
+    formulario.addEventListener('submit', function (event) {
+        let turnoId = document.querySelector('#turno_id').value;
+
+        const formData = {
+            id: document.querySelector('#turno_id').value,
+           odontologoId: document.querySelector('#odontologo_id').value,
+            pacienteId: document.querySelector('#paciente_id').value,
+            fecha: document.querySelector('#apellido').value,
+
+        };
+
+
+        const url = '/turnos';
+        const settings = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        }
+          fetch(url,settings)
+          .then(response => response.json())
+
+    })
+ })
+
+
+    function findBy(id) {
+          const url = '/turnos'+"/"+id;
+          const settings = {
+              method: 'GET'
+          }
+          fetch(url,settings)
+          .then(response => response.json())
+          .then(data => {
+              let odontologo = data;
+              document.querySelector('#turno_id').value = turno.id;
+              document.querySelector('#odontologo_id').value = turno.odontologo;
+              document.querySelector('#paciente_id').value = turno.paciente;
+              document.querySelector('#fecha').value = turno.fecha;
+
+              document.querySelector('#div_turno_updating').style.display = "block";
+          }).catch(error => {
+              alert("Error: " + error);
+          })
+      }
